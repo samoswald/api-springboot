39dedcbd3f4839a704ee4fea871410e3db3f47c9
Aravind <124670314+samoswald@users.noreply.github.com>
2023-02-08 12:00:53 +0530

    update

--- src/main/java/com/example/restapi/RestapiSpringbootApplication.java
+++ src/main/java/com/example/restapi/RestapiSpringbootApplication.java
@@ -9,6 +9,7 @@ public class RestapiSpringbootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestapiSpringbootApplication.class, args);
 		System.out.println("Hello world");
+		System.out.println("test sample");
 	}
 
 }
