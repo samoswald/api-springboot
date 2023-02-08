ba8ec36727c8918ae686df64be566813d67fab13
samoswald <aka.samdev@gmail.com>
2023-02-08 11:36:45 +0530

    init-rest

---
+++ src/main/java/com/example/restapi/RestapiSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiSpringbootApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
